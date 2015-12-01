8e193d1ab88e9329e97876f4690471dcd6b586b0
Kevin Simper <kevin.simper@gmail.com>
2015-12-02 00:43:38 +0100

    Only use react for composing the layout

--- app/server.js
+++ app/server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var harp = require('harp');
 var app = express();
 
-var index = require('jade!./index.jade')
-var post = require('jade!./post.jade')
+var layout = require('jade!./layout.jade')
 
 var blogposts = require('./blog/posts/_data.json')
 
@@ -14,24 +13,68 @@ app.use(express.static(process.cwd() + '/public'));
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+
 var sidebar = renderToString(<Sidebar/>)
 
 app.get('/', (req, res) => {
-  res.send(index({
-    posts: blogposts,
-    production: production,
-    sidebar: sidebar
+  let content = renderToString(
+    <div>
+      <div className="pure-g">
+        <div className="pure-u-1-4 sidebar">
+          <Sidebar/>
+        </div>
+      </div>
+      <div className="pure-u-3-4 content">
+        <div className="innercontent">
+          <div className="pure-u-5-6">
+            <Content>
+              {blogposts.map(post =>
+                <a className='articlelink' href={'/posts/' + post.slug}>
+                  <i className='date'>{post.date}</i>
+                  <h3 className='title'>{post.title}</h3>
+                </a>
+              )}
+            </Content>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  res.send(layout({
+    content: content,
+    production: production
   }))
 })
 
 app.get('/posts/:post', (req, res) => {
   var slug = req.params.post
   var blogpost = blogposts.find(item => item.slug === slug)
-  var content = require('./blog/posts/' + slug + '.md')
-  res.send(post({
+  var blogcontent = require('./blog/posts/' + slug + '.md')
+
+  let content = renderToString(
+    <div>
+      <div className="pure-g">
+        <div className="pure-u-1-4 sidebar">
+          <Sidebar/>
+        </div>
+      </div>
+      <div className="pure-u-3-4 content">
+        <div className="innercontent">
+          <div className="pure-u-5-6">
+            <Content>
+              <div dangerouslySetInnerHTML={{__html: blogcontent}}></div>
+            </Content>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  res.send(layout({
     content: content,
-    production: production,
-    sidebar: sidebar
+    production: production
   }))
 })
 
