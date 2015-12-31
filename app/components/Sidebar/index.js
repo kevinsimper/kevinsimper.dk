5c4735cca290dd0fc51b6c98c422874aebcf72e9
Kevin Simper <kevin.simper@gmail.com>
2015-12-31 19:00:50 +0100

    Add a proper menu for better navigation

--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -5,26 +5,18 @@ export default class Sidebar extends React.Component {
   render() {
     return (
       <div className={styles.sidebar}>
-        <img className={styles.profilepic} src='https://2.gravatar.com/avatar/b120b6c94e16f352c691849391324206?s=440'/>
+        <a href='/'><img className={styles.profilepic} src='https://2.gravatar.com/avatar/b120b6c94e16f352c691849391324206?s=440'/></a>
         <h1 className={styles.title}>
           <a href='/'>Kevin Simper</a>
         </h1>
         <h3 className={styles.subtitle}>
           JavaScript, Crafty.js and other coding.
         </h3>
-        <div  className={styles.links}>
-          <div className={styles.link}>
-            <a href='https://github.com/kevinsimper'>Github</a>
-          </div>
-          <div className={styles.link}>
-            <a href='https://twitter.com/kevinsimper'>Twitter</a>
-          </div>
-          <div className={styles.link}>
+        <div className={styles.Menu}>
+           <a href='https://github.com/kevinsimper'>Github</a>
+          <a href='https://twitter.com/kevinsimper'>Twitter</a>
           <a href='https://www.facebook.com/kevinsimper'>Facebook</a>
-          </div>
-          <div className={styles.link}>
-            <a href='https://linkedin.com/in/kevinsimper'>LinkedIn</a>
-          </div>
+          <a href='https://linkedin.com/in/kevinsimper'>LinkedIn</a>
         </div>
       </div>
     )
