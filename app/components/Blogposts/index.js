8ea6c65aaa50289d8a7cb413f3a149942c44b3cd
Kevin Simper <kevin.simper@gmail.com>
2016-02-19 22:10:43 +0100

    A longer introduction

--- app/components/Blogposts/index.js
+++ app/components/Blogposts/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from './style.scss'
 
 export default class Blogposts extends Component {
   render() {
     return (
-      <div>
-        <h2>Blogposts</h2>
+      <div className={styles.Blogposts}>
+        <h2 className={styles.Header}>Blogposts</h2>
         {this.props.blogposts.map(post =>
           <a className='articlelink' href={'/posts/' + post.slug}>
           <i className='date'>{post.date}</i>
--- app/components/HelloWorld/index.js
+++ app/components/HelloWorld/index.js
@@ -5,7 +5,12 @@ export default class HelloWorld extends React.Component {
   render() {
     return (
       <div className={styles.HelloWorld}>
-        Hi 😄, my name is <strong>Kevin Simper</strong> 🤓 and I am a full stack developer. I really like <strong>React.js</strong> 🚀 and <strong>Docker</strong> 🐳.
+        <div className={styles.Intro}>
+          Hi 😄, my name is <strong>Kevin Simper</strong> 🤓 and I am a full stack developer. I really like <strong>React.js</strong> 🚀 and <strong>Docker</strong> 🐳.
+        </div>
+        <div className={styles.SecondIntro}>
+          I also like to organize meetups and make <a href="/about">presentations</a> 💻. I really like to snowboard 🏂, and bicycles is my prefered transportation 🚴.
+        </div>
       </div>
     )
   }
