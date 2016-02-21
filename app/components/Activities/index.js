45566f1e6a5f0f84832dbb06f09b81244ba9db50
Kevin Simper <kevin.simper@gmail.com>
2016-02-21 16:20:58 +0100

    Make posts published on medium look different

--- app/components/Activities/index.js
+++ app/components/Activities/index.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import data from './data.json'
+import styles from './style.scss'
+import moment from 'moment'
+import mediumLogo from './medium.png'
 
 export default class Activities extends Component {
   render() {
     return (
       <div>
         <h2>My latest activities</h2>
-        {data.activities.map(post =>
-          <div style={{marginBottom: 10}}>
-            <a className="m-story" data-width="100%" data-collapsed="true" href={post.link}>No, I will not cross-promote your event!</a>
+        {data.activities.map((post, key) =>
+          <div className={styles.Post} key={key}>
+            <div className={styles.Date}>{post.date} - {moment(post.date).fromNow()}</div>
+            <h3 className={styles.Header}>
+              <a href={post.link}>{post.title}</a>
+            </h3>
+            <div>Published on <img src={mediumLogo} className={styles.MediumLogo}/> Medium</div>
           </div>
         )}
-        <script async src="https://static.medium.com/embed.js"></script>
       </div>
     )
   }
