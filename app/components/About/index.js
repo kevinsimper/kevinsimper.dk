77323c509d6d8584d6387eb5ad71e3e319a5edb6
Kevin Simper <kevin.simper@gmail.com>
2016-07-27 21:34:49 +0200

    Limit the width of page, else the lines would be to long

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Presentations from '../Presentations'
 import WorkExperience from '../WorkExperience'
 import Groups from '../Groups'
+import styles from './style.scss'
 
 export default class About extends React.Component {
   render() {
     return (
       <div>
         <h1>About</h1>
-        <Presentations/>
-        <WorkExperience/>
-        <Groups/>
+        <div className={styles.Content}>
+          <Presentations/>
+          <WorkExperience/>
+          <Groups/>
+        </div>
       </div>
     )
   }
