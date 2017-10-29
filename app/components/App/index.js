2cc4d84e36310a94c435f90c9ac643fef426c4cd
Kevin Simper <kevin.simper@gmail.com>
2017-10-30 00:40:13 +0100

    Add Header menu

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import Footer from '../Footer'
+import Header from '../Header'
 import styles from './style.scss'
 
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.App}>
-        <div className={styles.Header}>
-          <div className={styles.HeaderInner}>
-            <h1>
-              <a href="/">Kevin Simper</a>
-            </h1>
-            <div className={styles.Menu}>
-              <a href="/">Home</a>
-              <a href="/about">About & CV</a>
-              <a href="/contact">Contact</a>
-              <a href="/map">Travel Map</a>
-              <a href="https://github.com/kevinsimper">Github</a>
-              <a href="https://twitter.com/kevinsimper">Twitter</a>
-              <a href="https://www.facebook.com/kevinsimper">Facebook</a>
-              <a href="https://linkedin.com/in/kevinsimper">LinkedIn</a>
-            </div>
-          </div>
-        </div>
+        <Header/>
         <div>{this.props.children}</div>
         <Footer/>
       </div>
---
+++ app/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './style.scss'
+
+export default function Header() {
+  return (
+    <div className={styles.Header}>
+      <div className={styles.HeaderInner}>
+        <h1>
+          <a href="/">Kevin Simper</a>
+        </h1>
+        <div className={styles.Menu}>
+          <a href="/">Home</a>
+          <a href="/about">About & CV</a>
+          <a href="/contact">Contact</a>
+          <a href="/map">Travel Map</a>
+          <a href="https://github.com/kevinsimper">Github</a>
+          <a href="https://twitter.com/kevinsimper">Twitter</a>
+          <a href="https://www.facebook.com/kevinsimper">Facebook</a>
+          <a href="https://linkedin.com/in/kevinsimper">LinkedIn</a>
+        </div>
+      </div>
+    </div>
+  )
+}
