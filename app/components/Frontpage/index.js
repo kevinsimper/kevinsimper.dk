648435eef13d10900af1227e404fc23b5c071ee7
Kevin Simper <kevin.simper@gmail.com>
2018-04-02 15:50:16 +0200

    Adding videoes to the frontpage

--- app/components/Frontpage/index.js
+++ app/components/Frontpage/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import HelloWorld from '../HelloWorld'
 import Activities from '../Activities'
 import Instagram from '../Instagram'
+import Content from '../Content'
 import styles from './style.scss'
 
 export default class Frontpage extends Component {
@@ -10,10 +11,44 @@ export default class Frontpage extends Component {
       <div className={styles.Frontpage}>
         <div className={styles.Content}>
           <HelloWorld />
-          <Activities limit={10} blogposts={this.props.blogdata} />
-          <div style={{textAlign: 'right'}}>
-            <a href={'/activities'}>See the full list of activities →</a>
+          <Content>
+            <h2>My latest travels</h2>
+            <iframe
+              src="/map"
+              height="300px"
+              width="100%"
+              style={{ border: 'none', marginTop: 20 }}
+            />
+          </Content>
+          <div style={{ background: '#7FDBFF' }}>
+            <Content>
+              <h2 style={{ color: 'hsla(197, 100%, 20%, 1.0)' }}>
+                My latest videoes
+              </h2>
+              <div className={'embed'} style={{ margin: '20px 0' }}>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/eg4e-FObyJ8"
+                  frameBorder="0"
+                />
+              </div>
+              <div className={'embed'}>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/mLJh4lt4664"
+                  frameBorder="0"
+                />
+              </div>
+            </Content>
           </div>
+          <Content>
+            <Activities limit={10} blogposts={this.props.blogdata} />
+            <div style={{ textAlign: 'right' }}>
+              <a href={'/activities'}>See the full list of activities →</a>
+            </div>
+          </Content>
         </div>
       </div>
     )
