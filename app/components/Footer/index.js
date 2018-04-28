51b52329a39c747eb48b98d587114209fd8173ed
Kevin Simper <kevin.simper@gmail.com>
2018-04-29 00:41:34 +0200

    Add links to footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -4,8 +4,22 @@ import styles from './style.scss'
 export default function Footer() {
   return (
     <div className={styles.Footer}>
-      Contact me, I love speaking to new people!<br />
-      <a href="/contact">Contact</a>
+      <div className={styles.Col}>
+        <div>
+          Contact me, I love speaking to new people!<br />
+          <a href="/contact">Contact</a>
+          <br />
+        </div>
+        <div style={{ textAlign: 'center' }}>2018</div>
+        <div>
+          <strong>Pages:</strong>
+          <div>
+            <a href="/kubernetes-training">Kubernetes Training</a>
+            <br />
+            <a href="/recommends">Recommends</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
