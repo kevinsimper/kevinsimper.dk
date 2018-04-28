182a1a431585c9c364e7d40fd6ae5f070c8308ce
Kevin Simper <kevin.simper@gmail.com>
2018-04-29 00:32:06 +0200

    Add recommends page

--- app/components/Contact/index.js
+++ app/components/Contact/index.js
@@ -5,7 +5,8 @@ export default class Contact extends Component {
   render() {
     return (
       <div>
-        <h1>Contact</h1>
+        <h1>Contact me</h1>
+        <p>You are very welcome to contact me.</p>
         <Contactform />
       </div>
     )
--- app/server.js
+++ app/server.js
@@ -183,4 +183,46 @@ app.get('/kubernetes-training', (req, res) => {
   )
 })
 
+app.get('/recommends', (req, res) => {
+  let content = renderToString(
+    <App>
+      <Content>
+        <h1>Stuff I recommend</h1>
+        <p>Recipes:</p>
+        <ul>
+          <li>
+            Pancakes - 20 pancakes
+            <ul>
+              <li>250 gram flour</li>
+              <li>3 eggs</li>
+              <li>6 dl milk</li>
+              <li>2 spoon sugar</li>
+              <li>1 nip salt</li>
+            </ul>
+          </li>
+          <li>
+            Pizza - 2 pizzas
+            <ul>
+              <li>0,5 yeast</li>
+              <li>3 lukewarm water</li>
+              <li>1 spoon oliveoil</li>
+              <li>0,5 tespoon salt</li>
+              <li>200 gram flour</li>
+              <li>100 gram durumflour</li>
+            </ul>
+          </li>
+        </ul>
+      </Content>
+    </App>
+  )
+  res.send(
+    layout({
+      content,
+      production,
+      assets,
+      title: 'Recommends - Kevin Simper'
+    })
+  )
+})
+
 module.exports = app
