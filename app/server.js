ed234955478699745be7c84aad743064891872e1
Kevin Simper <kevin.simper@gmail.com>
2018-04-30 13:54:46 +0200

    Move out into components

--- app/server.js
+++ app/server.js
@@ -14,6 +14,7 @@ import Frontpage from './components/Frontpage'
 import ActivitiesPage from './components/ActivitiesPage'
 import KubernetesTraining from './components/KubernetesTraining'
 import { get } from 'axios'
+import Recommends from './components/Recommends'
 
 var app = express.Router()
 var production = process.env.NODE_ENV === 'production'
@@ -186,33 +187,7 @@ app.get('/kubernetes-training', (req, res) => {
 app.get('/recommends', (req, res) => {
   let content = renderToString(
     <App>
-      <Content>
-        <h1>Stuff I recommend</h1>
-        <p>Recipes:</p>
-        <ul>
-          <li>
-            Pancakes - 20 pancakes
-            <ul>
-              <li>250 gram flour</li>
-              <li>3 eggs</li>
-              <li>6 dl milk</li>
-              <li>2 spoon sugar</li>
-              <li>1 nip salt</li>
-            </ul>
-          </li>
-          <li>
-            Pizza - 2 pizzas
-            <ul>
-              <li>0,5 yeast</li>
-              <li>3 lukewarm water</li>
-              <li>1 spoon oliveoil</li>
-              <li>0,5 tespoon salt</li>
-              <li>200 gram flour</li>
-              <li>100 gram durumflour</li>
-            </ul>
-          </li>
-        </ul>
-      </Content>
+      <Recommends />
     </App>
   )
   res.send(
