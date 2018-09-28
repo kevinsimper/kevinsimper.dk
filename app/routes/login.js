cbd58de468f1aaff901acd5540609eaecc76b5ad
Kevin Simper <kevin.simper@gmail.com>
2018-09-28 02:50:02 +0200

    Add accountkit login

--- app/routes/login.js
+++ app/routes/login.js
@@ -4,26 +4,35 @@ import { renderToString } from 'react-dom/server'
 import App from '../components/App'
 import Content from '../components/Content'
 import { layout, production, assets } from '../server'
+import { get } from 'axios'
 let app = express.Router()
+
+const prod = process.env.NODE_ENV === 'production'
+const fbappid = '196260264266521'
+const secret = process.env.ACCOUNTKIT_SECRET
+const redirect = prod ? 'https://www.kevinsimper.dk/login/accountkit' : 'http://localhost:9000/login/accountkit'
+const accountkitToken = `AA|${fbappid}|${secret}`
 
 app.get('/', (req, res) => {
   let content = renderToString(
     <App>
       <Content>
         <h1>Login</h1>
-        <div>
-          <label>
-            <input type="text" placeholder="username" name="username" />
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="password" placeholder="password" name="password" />
-          </label>
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
+        <form method="get" action="https://www.accountkit.com/v1.0/basic/dialog/email_login/">
+          <input type="hidden" name="app_id" value="196260264266521"/>
+          <input type="hidden" name="redirect" value={redirect}/>
+          <input type="hidden" name="state" value="asdfsasd"/>
+          <input type="hidden" name="fbAppEventsEnabled" value="true"/>
+          <input type="hidden" name="debug" value="true"/>
+          <div>
+            <label>
+              <input type="email" placeholder="Email" name="email" />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
       </Content>
     </App>
   )
@@ -35,6 +44,18 @@ app.get('/', (req, res) => {
       title: 'Login - Kevin Simper'
     })
   )
+})
+
+app.get('/accountkit',  (req, res) => {
+  const { code, state, status } = req.query
+  get(`https://graph.accountkit.com/v1.1/access_token?grant_type=authorization_code&code=${code}&access_token=${accountkitToken}`).then(response => {
+    const { id, access_token } = response.data
+    console.log(access_token)
+    return get(`https://graph.accountkit.com/v1.3/me/?access_token=${access_token}`)
+  }).then(response => {
+    const email = response.data.email.address
+    res.send('Hi ' + email)
+  }).catch(e => console.log(e))
 })
 
 export default app
