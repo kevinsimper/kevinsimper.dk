dbe6864bf73e66562bad504e06d99924ddb3796e
Kevin Simper <kevin.simper@gmail.com>
2018-11-28 23:35:31 +0100

    Easily add a new post tool

---
+++ newpost.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const title = process.argv[2]
+const slug = title.toLowerCase().replace(/ /g, '-')
+const file = './app/blog/posts/_data.json'
+
+console.log('New Post')
+console.log('Title:', title)
+console.log('Slug:', slug)
+
+let posts = JSON.parse(fs.readFileSync(file))
+
+posts.unshift({
+  slug,
+  title,
+  date: new Date().toString(),
+  tags: []
+})
+
+fs.writeFileSync(file, JSON.stringify(posts, null, 2))
