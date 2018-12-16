16d96944325580d3d9789b7e5e51020fe0e95c66
Kevin Simper <kevin.simper@gmail.com>
2018-12-17 00:53:15 +0100

    Update recipies page

--- app/components/Recommends/index.js
+++ app/components/Recommends/index.js
@@ -1,47 +1,48 @@
 import React from 'react'
 import Content from '../Content'
 
+const Item = ({ children }) => (
+  <li style={{ listStyleType: 'none' }}>
+    <label>
+      <input style={{ fontSize: 20, verticalAlign: 2 }} type="checkbox" />{' '}
+      {children}
+    </label>
+  </li>
+)
+
 export default () => (
   <Content>
     <h1>Stuff I recommend</h1>
     <p>Recipes:</p>
+    <h3>Pancakes - 20 pancakes</h3>
     <ul>
-      <li>
-        Pancakes - 20 pancakes
-        <ul>
-          <li>250 gram flour</li>
-          <li>3 eggs</li>
-          <li>6 dl milk</li>
-          <li>2 spoon sugar</li>
-          <li>1 nip salt</li>
-        </ul>
-      </li>
-      <li>
-        Pizza - 2 pizzas
-        <ul>
-          <li>0,5 yeast</li>
-          <li>3 lukewarm water</li>
-          <li>1 spoon oliveoil</li>
-          <li>0,5 tespoon salt</li>
-          <li>200 gram flour</li>
-          <li>100 gram durumflour</li>
-        </ul>
-      </li>
-      <li>
-        Lasagne - 4 persons
-        <ul>
-          <li>500 grounded beef</li>
-          <li>1 onion</li>
-          <li>1 tomato puree</li>
-          <li>1 cut tomato</li>
-          <li>1 beef boulion</li>
-          <li>0,5 liter water</li>
-          <li>2 carrots</li>
-          <li>Salt/Paper</li>
-          <li>Cheese ontop</li>
-          <li>Lasagne plates</li>
-        </ul>
-      </li>
+      <Item>250 gram flour</Item>
+      <Item>3 eggs</Item>
+      <Item>6 dl milk</Item>
+      <Item>2 spoon sugar</Item>
+      <Item>1 nip salt</Item>
+    </ul>
+    <h3>Pizza - 2 pizzas</h3>
+    <ul>
+      <Item>0,5 yeast</Item>
+      <Item>3 lukewarm water</Item>
+      <Item>1 spoon oliveoil</Item>
+      <Item>0,5 tespoon salt</Item>
+      <Item>200 gram flour</Item>
+      <Item>100 gram durumflour</Item>
+    </ul>
+    <h3>Lasagne - 4 persons</h3>
+    <ul>
+      <Item>500 grounded beef</Item>
+      <Item>1 onion</Item>
+      <Item>1 tomato puree</Item>
+      <Item>1 cut tomato</Item>
+      <Item>1 beef boulion</Item>
+      <Item>0,5 liter water</Item>
+      <Item>2 carrots</Item>
+      <Item>Salt/Paper</Item>
+      <Item>Cheese ontop</Item>
+      <Item>Lasagne plates</Item>
     </ul>
   </Content>
 )
