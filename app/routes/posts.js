24fc463ba70bc5ff77b5f21d96bf63ca22c7ba64
Kevin Simper <kevin.simper@gmail.com>
2018-12-16 14:52:23 +0100

    Show quickly the previous and next blogpost

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -11,12 +11,26 @@ let app = express.Router()
 app.get('/:post', (req, res) => {
   var slug = req.params.post
   var blogpost = blogdata.find(item => item.slug === slug)
+  const previous = blogdata[blogdata.indexOf(blogpost) + 1]
+  const newer = blogdata[blogdata.indexOf(blogpost) - 1]
   import('../blog/posts/' + slug + '.md').then(blogcontent => {
     let content = renderToString(
       <App>
         <Content>
           <div>{blogpost.date}</div>
           <div dangerouslySetInnerHTML={{ __html: blogcontent.default }} />
+          <hr />
+          {previous && (
+            <p>
+              Previous post:{' '}
+              <a href={`/posts/${previous.slug}`}>{previous.title}</a>
+            </p>
+          )}
+          {newer && (
+            <p>
+              Newer post: <a href={`/posts/${newer.slug}`}>{newer.title}</a>
+            </p>
+          )}
         </Content>
       </App>
     )
