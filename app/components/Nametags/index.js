4cfc569e47fe0ae6d47372fcd9ea8214b688c294
Kevin Simper <kevin.simper@gmail.com>
2019-02-02 22:31:31 +0100

    Update with latest events

--- app/components/Nametags/index.js
+++ app/components/Nametags/index.js
@@ -5,7 +5,8 @@ export default () => (
     <h1>Printing nametags</h1>
     <p>
       I have helped communities print nametags. It is a great way to help people
-      start conversations.
+      start conversations. It also allows for some interesting thing with
+      personlized QR codes on the nametags.
     </p>
     <h3>I have helped:</h3>
     <ul>
@@ -17,10 +18,14 @@ export default () => (
       <li>HackYourFuture</li>
       <li>GDG Cloud London</li>
       <li>JSLovers New Delhi</li>
+      <li>GDG Organizers Summit</li>
+      <li>React Copenhagen</li>
+      <li>Nordic Growth Hackers</li>
     </ul>
     <p>
       I can also help you! I really like seeing communities grow and become
       better.
     </p>
+    <img src="https://cdn-images-1.medium.com/max/1600/1*7yWCggscg_nF1t8s57iVvg.jpeg" />
   </div>
 )
