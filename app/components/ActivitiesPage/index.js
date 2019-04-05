1d6fdfddf3dae4c71429592956dbb29a377f1948
Kevin Simper <kevin.simper@gmail.com>
2019-04-05 16:53:58 -0700

    Add a counter for blogposts

--- app/components/ActivitiesPage/index.js
+++ app/components/ActivitiesPage/index.js
@@ -8,6 +8,9 @@ export default class ActivitiesPage extends Component {
     return (
       <div>
         <h2>My latest activities</h2>
+        <p>
+          I have written {this.props.blogdata.length} blogposts on this site.
+        </p>
         <Activities blogposts={this.props.blogdata} />
       </div>
     )
