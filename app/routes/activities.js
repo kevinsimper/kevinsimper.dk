bdcf7d0f9cf4c8633574af918d87f8864adf5a84
Kevin Simper <kevin.simper@gmail.com>
2019-12-06 19:17:03 +0100

    Replace blogdata with graphql

--- app/routes/activities.js
+++ app/routes/activities.js
@@ -1,30 +1,45 @@
 import { renderToString } from 'react-dom/server'
 import React from 'react'
 import express from 'express'
-
 import { layout, production, assets } from '../server'
+import { makeQuery } from './graphql'
 import ActivitiesPage from '../components/ActivitiesPage'
 import App from '../components/App'
 import Content from '../components/Content'
-import blogdata from '../blog/posts/_data.json'
 
 let app = express.Router()
 
-app.get('/', (req, res) => {
-  let content = renderToString(
-    <App>
-      <Content>
-        <ActivitiesPage blogdata={blogdata} />
-      </Content>
-    </App>
+app.get('/', (req, res, next) => {
+  const posts = makeQuery(
+    `
+      {
+        posts {
+          slug
+          title
+          date
+          tags
+        }
+      }
+    `
   )
-  res.send(
-    layout({
-      content,
-      production,
-      assets,
-      title: 'All activities - Kevin Simper'
+    .then(graphRes => {
+      const posts = graphRes.data.posts
+      let content = renderToString(
+        <App>
+          <Content>
+            <ActivitiesPage blogdata={posts} />
+          </Content>
+        </App>
+      )
+      res.send(
+        layout({
+          content,
+          production,
+          assets,
+          title: 'All activities - Kevin Simper'
+        })
+      )
     })
-  )
+    .catch(next)
 })
 export default app
