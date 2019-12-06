9a68bb0278c47f41c1521e8d37e7140bce4bd180
Kevin Simper <kevin.simper@gmail.com>
2019-12-06 19:32:29 +0100

    Remove unused imports

--- app/server.js
+++ app/server.js
@@ -1,26 +1,20 @@
-import { renderToString } from 'react-dom/server'
-import React from 'react'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import express from 'express'
-
+import { NameTagsRoute } from './routes/nametags'
 import { assets, production, layout, map } from './consts.js'
 import { homeRoute } from './routes/home.js'
 import { server } from './routes/graphql'
 import AboutRoutes from './routes/about'
 import ActivitiesRoutes from './routes/activities'
-import App from './components/App'
 import CategoriesRoutes from './routes/categories'
 import ContactRoutes from './routes/contact'
-import Content from './components/Content'
 import FeedRoutes from './routes/feed'
 import KubernetesTrainingRoutes from './routes/kubernetes-training'
 import LoginRoutes from './routes/login'
 import MapRoutes from './routes/map'
-import Nametags from './components/Nametags'
 import PostsRoutes from './routes/posts'
 import RecommendsRoutes from './routes/recommends'
-import { NameTagsRoute } from './routes/nametags'
 
 export { assets, production, layout, map }
 var app = express.Router()
