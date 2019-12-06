6cb3a160a2c98c89f65f65acccc979bb49aade40
Kevin Simper <kevin.simper@gmail.com>
2019-12-06 19:10:18 +0100

    Remove accountkit

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,7 +2,6 @@ import { get } from 'axios'
 import { renderToString } from 'react-dom/server'
 import React from 'react'
 import express from 'express'
-
 import { layout, production, assets } from '../server'
 import App from '../components/App'
 import Content from '../components/Content'
@@ -10,27 +9,13 @@ import Content from '../components/Content'
 let app = express.Router()
 
 const prod = process.env.NODE_ENV === 'production'
-const fbappid = '196260264266521'
-const secret = process.env.ACCOUNTKIT_SECRET
-const redirect = prod
-  ? 'https://www.kevinsimper.dk/login/accountkit'
-  : 'http://localhost:9000/login/accountkit'
-const accountkitToken = `AA|${fbappid}|${secret}`
 
 app.get('/', (req, res) => {
   let content = renderToString(
     <App>
       <Content>
         <h1>Login</h1>
-        <form
-          method="get"
-          action="https://www.accountkit.com/v1.0/basic/dialog/email_login/"
-        >
-          <input type="hidden" name="app_id" value="196260264266521" />
-          <input type="hidden" name="redirect" value={redirect} />
-          <input type="hidden" name="state" value="asdfsasd" />
-          <input type="hidden" name="fbAppEventsEnabled" value="true" />
-          <input type="hidden" name="debug" value="true" />
+        <form method="get">
           <div>
             <label>
               <input type="email" placeholder="Email" name="email" />
@@ -51,24 +36,6 @@ app.get('/', (req, res) => {
       title: 'Login - Kevin Simper'
     })
   )
-})
-
-app.get('/accountkit', (req, res) => {
-  const { code, state, status } = req.query
-  const accessToken = `https://graph.accountkit.com/v1.1/access_token?grant_type=authorization_code&code=${code}&access_token=${accountkitToken}`
-  get(accessToken)
-    .then(response => {
-      const { id, access_token } = response.data
-      const meUrl = `https://graph.accountkit.com/v1.3/me/?access_token=${access_token}`
-      return get(meUrl)
-    })
-    .then(response => {
-      const email = response.data.email.address
-      req.signedCookies.email = email
-      res.cookie('id', email, { signed: true })
-      res.redirect('/login/dashboard')
-    })
-    .catch(e => console.log(e))
 })
 
 app.get('/dashboard', (req, res) => {
