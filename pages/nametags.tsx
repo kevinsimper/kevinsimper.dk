9dea57e65c58c99fc39f96314d5ae6bb3f3d9e91
Kevin Simper <kevin.simper@gmail.com>
2020-12-28 00:20:08 +0100

    Add page about nametags

---
+++ pages/nametags.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>Printing nametags</h1>
+    <p>
+      I have helped communities print nametags. It is a great way to help people
+      start conversations. It also allows for some interesting thing with
+      personlized QR codes on the nametags.
+    </p>
+    <h3>I have helped:</h3>
+    <ul>
+      <li>Ruby Brigade</li>
+      <li>CopenhagenJS</li>
+      <li>SFNode</li>
+      <li>Copenhagen Ethereum</li>
+      <li>FoundersHouse Fridaybar</li>
+      <li>HackYourFuture</li>
+      <li>GDG Cloud London</li>
+      <li>JSLovers New Delhi</li>
+      <li>GDG Organizers Summit</li>
+      <li>React Copenhagen</li>
+      <li>Nordic Growth Hackers</li>
+    </ul>
+    <p>
+      I can also help you! I really like seeing communities grow and become
+      better.
+    </p>
+    <p>
+      <a href="https://medium.com/@kevinsimper/how-to-print-name-tags-for-your-meetup-c6293a8faf8f">
+        A bit how I initially made the labelprinter work
+      </a>
+      .
+    </p>
+    <img src="https://cdn-images-1.medium.com/max/1600/1*7yWCggscg_nF1t8s57iVvg.jpeg" />
+  </div>
+)
