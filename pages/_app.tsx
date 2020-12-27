2c3a3bd04c437782da1d8703e549c210053f836f
Kevin Simper <kevin.simper@gmail.com>
2020-12-28 00:45:53 +0100

    Add links in footer and kubernetes training page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps }) {
       </div>
       <footer className="bg-indigo-900 text-white p-8">
         <div className="max-w-screen-md mx-auto flex justify-around">
-          <a href="/social">Contact me</a>
+          <a href="/social">Contact me!</a>
           <form method="GET" action="/search">
             <input
               className="p-2"
@@ -50,6 +50,30 @@ function MyApp({ Component, pageProps }) {
               placeholder="Search.."
             />
           </form>
+          <div>
+            <strong>Pages:</strong>
+            <div>
+              <a href="/nametags">Nametags</a>
+              <br />
+              <a href="/kubernetes-training">Kubernetes Training</a>
+              <br />
+              <a href="/recommends">Recommends</a>
+              <br />
+              <a href="/projects">Projects</a>
+              <br />
+              <a href="/contact">Contact</a>
+              <br />
+              <a href="https://talkfrom.com">Talkfrom</a>
+              <br />
+              <a href="http://kevinsimper.eth">kevinsimper.eth</a>
+              <br />
+              <a href="https://gdg-search.firebaseapp.com">GDG Search</a>
+              <br />
+              <a href="https://copenhagenjs.dk/speaker/?name=kevin-simper">
+                CopenhagenJS Speaker Profile
+              </a>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
