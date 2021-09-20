4ef73d2c7cdb0f4fa18fc1ef650c1009481e6420
Kevin Simper <kevin.simper@gmail.com>
2021-09-20 22:11:42 +0200

    Remove old server and pages

--- app/components/Content/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './styles.scss'
-
-export default class Sidebar extends React.Component {
-  render() {
-    return <div className={styles.Content}>{this.props.children}</div>
-  }
-}
