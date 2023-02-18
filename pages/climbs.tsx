7c41cf1f3e3f8aa2bde401fe4a6d47fece0d4349
Kevin Simper <kevin.simper@gmail.com>
2023-02-18 04:50:34 +0100

    Improve data for climbs with start and end

--- pages/climbs.tsx
+++ pages/climbs.tsx
@@ -2,51 +2,73 @@ import React from 'react'
 
 function ClimbsPage() {
   return (
-    <div>
-      <h1 className="text-2xl">Climbs</h1>
-      <p>Climbs that I have done on bike that I really liked</p>
+    <div className="space-y-4">
       <div>
+        <h1 className="text-2xl">Climbs</h1>
+        <p>Climbs that I have done on bike that I really liked</p>
+      </div>
+      <div className="grid gap-3">
         {[
           {
             name: "Alpe d'Heuz",
             country: 'France',
-            position: '45.0946915,6.0708381',
+            end: '45.0946915,6.0708381',
+            start: '45.065250, 6.040131',
+            length: 13000,
           },
           {
             name: 'Munkebjerg',
             country: 'Denmark',
-            position: '55.6858073,9.6172224',
+            end: '55.6858073,9.6172224',
+            start: '55.692863, 9.615131',
           },
           {
             name: 'Mont Ventoux',
             country: 'France',
-            position: '44.1740843,5.2699249',
+            end: '44.174084,5.269925',
+            start: '44.123232, 5.180506',
+            length: 22000,
           },
           {
             name: 'Sa Calobra',
             country: 'Mallorca',
-            position: '39.849081,2.7971858',
+            end: '39.816088,2.823908',
+            start: '39.851002,2.799172',
           },
           {
             name: 'Col du Galibier',
             country: 'France',
-            position: '45.0640978,6.398976',
+            end: '45.064061,6.407863',
+            start: '45.163654, 6.422708',
           },
           {
             name: 'Udsigten',
             country: 'Denmark',
-            position: '55.363653,11.3131511',
+            end: '55.370845,11.328069',
+            start: '55.363676,11.314370',
           },
           {
             name: 'Brocken',
             country: 'Germany',
-            position: '51.7994226,10.6064637',
+            end: '51.799100,10.615623',
+            start: '51.772229, 10.647465',
           },
         ].map((climb) => {
           return (
-            <div>
-              <a href={`https://www.google.com/maps/place/${climb.position}`}>
+            <div className="border p-4">
+              <a
+                href={`https://www.google.com/maps/place/${climb.end}`}
+                className="block"
+                target={'_blank'}
+              >
                 {climb.name} - {climb.country}
+              </a>
+              <a
+                href={`https://www.google.com/maps/dir/${climb.start}/${climb.end}`}
+                className="block"
+                target={'_blank'}
+              >
+                See route
               </a>
             </div>
           )
