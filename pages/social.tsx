be16e79cac5af1af2022328bf23ae6e3f3fcacfe
Kevin Simper <kevin.simper@gmail.com>
2023-02-28 05:02:13 +0100

    Improve social buttons

--- pages/social.tsx
+++ pages/social.tsx
@@ -9,36 +9,36 @@ function SocialPage() {
       </Head>
       <h1 className="text-2xl text-center">Find me on social media</h1>
       <ul className="grid lg:grid-cols-4 gap-3 my-4 content-center items-center">
-        <li className="text-center border-2 bg-black text-white rounded-full">
+        <li className="text-center bg-black text-white rounded-xl shadow-xl border">
           <a
             className="block aspect-square flex place-items-center"
             href="https://github.com/kevinsimper"
           >
-            <div className="flex-1">GitHub</div>
+            <div className="flex-1">👨‍💻 GitHub</div>
           </a>
         </li>
-        <li className="text-center border-2 bg-blue-400 text-white rounded-full">
+        <li className="text-center bg-blue-400 text-white rounded-xl shadow-xl border">
           <a
             className="block aspect-square flex place-items-center"
             href="https://twitter.com/kevinsimper"
           >
-            <div className="flex-1">Twitter</div>
+            <div className="flex-1">🐦 Twitter</div>
           </a>
         </li>
-        <li className="text-center border-2 rounded-full">
+        <li className="text-center rounded-xl shadow-xl border">
           <a
             className="block aspect-square flex place-items-center"
             href="https://linkedin.com/in/kevinsimper"
           >
-            <div className="flex-1">LinkedIn</div>
+            <div className="flex-1">👨‍💼 LinkedIn</div>
           </a>
         </li>
-        <li className="text-center border-2 bg-red-600 text-white rounded-full">
+        <li className="text-center bg-red-600 text-white rounded-xl shadow-xl border">
           <a
             className="block aspect-square flex place-items-center"
             href="https://www.youtube.com/channel/UCarQbOR6-lCqaa0d_MB9iGQ"
           >
-            <div className="flex-1">YouTube</div>
+            <div className="flex-1">🎥 YouTube</div>
           </a>
         </li>
       </ul>
